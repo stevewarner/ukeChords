0ced8ffbd0a5f9a17715905290835b1f60fa3068
Steven Warner <StevenWarner@Stevens-Mac-Pro.local>
2018-11-27 21:03:05 -0800

    Updates

---
+++ precache-manifest.a8551fda563d1e389757746297275d77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "603473b3cc06e6227e2b77fa0db0915c",
+    "url": "/ukeChords/static/media/F.603473b3.png"
+  },
+  {
+    "revision": "4b8b1c64d902731f1ede",
+    "url": "/ukeChords/static/js/runtime~main.4b8b1c64.js"
+  },
+  {
+    "revision": "27974ae78e714ecf62bb",
+    "url": "/ukeChords/static/js/main.27974ae7.chunk.js"
+  },
+  {
+    "revision": "024b46d994aa423462a6",
+    "url": "/ukeChords/static/js/1.024b46d9.chunk.js"
+  },
+  {
+    "revision": "27974ae78e714ecf62bb",
+    "url": "/ukeChords/static/css/main.48700b0f.chunk.css"
+  },
+  {
+    "revision": "1297ffa687a986a3ae201a62b1bd6eb5",
+    "url": "/ukeChords/index.html"
+  }
+];
